57be6719e544a1fc9c338868381ad77b5ca17de7
Zia Uddin <ziawc0001@gmail.com>
2025-03-22 18:34:32 +0000

    conversion of datatype

---
+++ basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "443bcsj"
+
+// console.log(typeof score);
+
+ let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"443" => 443
+//443bshj => Nan
+//true => 1
+//null => 0
+//undefined => Nan
+
+let isLoggedIn = 1
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
