2132d9024c07ac65e47e3fb180630092e0cb50d1
Zia Uddin <ziawc0001@gmail.com>
2025-03-22 17:49:16 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer versiom
+//alert(1 + 1); //we are using nodejs, not browser
+
+
+let name = "zia"      //string
+
+let age = 23          //number  => 2 to power 53
+
+let isLogedIn = false //boolean => true/false
+                    
+//null => standalone value //type => object
+
+//undefined => value not assigned
+
+//symbol => unique
+
+//object
+
+console.log(typeof null);
