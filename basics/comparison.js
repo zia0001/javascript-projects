a871f33e652899457f263e18805018a815938418
Zia Uddin <ziawc0001@gmail.com>
2025-03-22 19:53:30 +0000

    comparison in js

---
+++ basics/comparison.js
@@ -0,0 +1,27 @@
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// let marks;
+// console.log(marks);
+
+/*  console.log(null > 0); // false
+Comparison (>) converts null to a number before comparing.
+
+null is converted to 0.
+
+0 > 0 is false.
+
+2. console.log(null == 0); // false
+The equality check (==) does not convert null to a number.
+
+null is only equal to undefined in loose equality (==), not to numbers.
+
+So, null == 0 is false.
+
+3. console.log(null >= 0); // true
+The >= comparison also converts null to a number (0).
+
+It becomes 0 >= 0, which is true. */
+
+console.log("2" > 1);
