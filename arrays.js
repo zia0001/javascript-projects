80a8ebc63dda64646c1ee4cb61da596fe5917971
Zia Uddin <ziawc0001@gmail.com>
2025-03-31 21:12:58 +0000

    arrays and other files

---
+++ arrays.js
@@ -0,0 +1,48 @@
+// let myArray = [3, 4, 6, 98, 100]
+// console.log(myArray[3]);
+
+// let myfellows = ["hadi", "khalid", "suultan"]
+// myfellows = myArray
+
+// console.log(myfellows[2]);
+
+
+// console.log([myArray[3], myfellows[1]]);
+
+// ************* Methods*****************
+// myArr2 = [3, 4, 76, 89]
+// myArr2.push(88)    //add element to last of array
+// myArr2.push(1098)
+// myArr2.pop()       //removes last element from array
+// myArr2.unshift(77)  //add element to begning of array
+// myArr2.shift()      // removes first element of array
+// myArr2.shift()
+
+// console.log(myArr2);
+// console.log(myArr2.includes(76)); //ask questions from array whether the element is present in it or not
+// console.log(myArr2.indexOf(76)) //shows index of element in array
+
+// const newArr = myArr2.join()  //joins two arrays
+// console.log(myArr2);
+// console.log(newArr);  //its type is string
+
+
+const myarn1 = [44, 55, 66, 77, 999]
+console.log(myarn1);
+const mry1 = myarn1.slice(1, 4)   // returns selected elements in an array, as a new array
+console.log(mry1);
+
+const mry2 = myarn1.splice(1, 4)  // used to remove, replace or insert items starting at a chosen index
+console.log(mry2);
+console.log(myarn1);
+
+
+
+
+
+
+
+
+
+
+
